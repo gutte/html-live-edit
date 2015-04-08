a9d541bd624d8267023969880c788212a0d231c5
Gustav Klingstedt <gustav.klingstedt@gmail.com>
2015-04-08 14:13:30 +0200

    using contentEditable instead of forms. also some keydown events for enter and esc while in editable.

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 Live Editor for simple HTML documents.
 
-Document contents are changed through clicking and editing in simple dialogs.
-Basic HTML text elements (h1-h6 & p) can be clicked and edited through form inputs (jQuery).
-Editable elements must be of class "edit" and inside article#main.
+Article contents are changed through clicking and editing (using contentEditable property).
+Editable elements must be of class "editable" and inside article#main.
 
-Changes are sent to the server which instantly broadcasts to all clients using websocket and node.js server.
+Changes are sent to the server which instantly broadcasts the same to all clients.
+
+Using websocket and node.js server.
 
--- public/js/client.js
+++ public/js/client.js
@@ -1,44 +1,48 @@
 var socket;
 $(document).ready(function($) {
-    $(document).on("click","article#main .edit",function (event) {
-        var eid = event.target.id;
-        var etag = event.target.tagName;
-        var evalue = $( "#"+eid ).html();
-        if (etag == "P") {
-            $("#"+eid).replaceWith( '<form id="'+eid+'" class="edit-form">'
-                +'<textarea rows="5" cols="35" class="edit-input"></textarea>'
-                +'<input type="hidden" name="etag" value="'+etag+'" class="etag">'
-                +'<input type="submit" value="Edit">'
-                +'</form>' ); 
-            $("#"+eid+" .edit-input").trigger( "focus" );
-            $("#"+eid+" .edit-input" ).html(evalue);
-        } else {
-            $("#"+eid).replaceWith( '<form id="'+eid+'" class="edit-form">'
-                +'<input type="text" size="20" class="edit-input" value="">'
-                +'<input type="hidden" name="etag" value="'+etag+'" class="etag">'
-                +'<input type="submit" value="Edit">'
-                +'</form>' );
-            $("#"+eid+" .edit-input").trigger( "focus" );
-            $("#"+eid+" .edit-input").val(evalue)
-        }
-    });
-    $(document).on("submit", "article#main form.edit-form", function (event) {
-        var eid = event.target.id;
-        var etag = $("form#"+eid+" .etag").val();
-        //send to server
-        var sendData = {
-            eid : eid,
-            etag : etag,
-            val : $("form#"+eid+" .edit-input").val()
+
+    $(document).on("focus","article#main .editable",function (event) {
+        var ee = {
+            id: event.target.id,
+            tagName: event.target.tagName,
+            oldHTML: $( event.target ).html(),
+            newHTML: ""
         };
-        console.log("Sending data");
-        socket.send( JSON.stringify(sendData) );
-        // set pending gif
-        // later
-        // sleep and check if updated
-        // later
-        event.preventDefault();
+        //console.log('focus '+ee.id);
+        
+        //lock element globally
+        //later
+
+        // listener for blur event
+        $(document).on("blur",".editable",function (event) {
+            //console.log('blur '+ee.id);
+            ee.newHTML = $( "#"+ee.id ).html();
+            if (ee.oldHTML == ee.newHTML) {
+                
+            } else {
+                //Sending
+                console.log('Sending..');
+                socket.send( JSON.stringify(ee) );
+            };
+        });
+        
     });
+    
+    $('.editable').on('keydown', function(event) {
+        if (event.which == 13) { //Enter
+            // open up new paragraph
+            $(event.target).trigger('blur'); // temporary
+            return false; // prevents default action
+        }
+        if (event.which == 27) { //Esc
+            // blur focus
+            $(event.target).trigger('blur');
+            return false; // prevents default action
+        }
+        
+        // Do some arrow keys
+    });
+    
     
     socket = new WebSocket("ws://127.0.0.1:8081");
     
@@ -54,11 +58,8 @@ $(document).ready(function($) {
     };
     socket.onmessage = function(event) {
         console.log("Received data");
-        var data = JSON.parse(event.data);
-        var eid = data.eid;
-        var etag = data.etag;
-        var val = data.val;
-        $("#"+eid).replaceWith ( '<'+etag+' id="'+eid+'" class="edit">'+val+'</'+etag+'>' );
+        var ee = JSON.parse(event.data);
+        $("#"+ee.id).html ( ee.newHTML );
         event.preventDefault();
     };
 });
